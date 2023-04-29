71193e0d718d8760ee8f07da4d13c7b3e3132563
unknown <axelnperez@hotmail.com>
2023-04-29 21:12:38 +0200

    feat: Added a lot of feature.
    - Added flip sprite when player moves.
    - Added jump.
    - Added climbing and climbing animation.
    - Added cameras for idling, running and climbing.

--- Proyectos/TileVania/Assets/Scripts/PlayerMovement.cs
+++ Proyectos/TileVania/Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,78 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 20f;
+    [SerializeField] float climbSpeed = 1f;
+
     Vector2 moveInput;
+    Rigidbody2D rigidBody;
+    Animator animator;
+    CapsuleCollider2D collider;
+    float originalGravity;
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        collider = GetComponent<CapsuleCollider2D>();
+        originalGravity = rigidBody.gravityScale;
     }
 
     void Update()
     {
-        
+        Run();
+        FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!(collider.IsTouchingLayers(LayerMask.GetMask("Ground"))||collider.IsTouchingLayers(LayerMask.GetMask("Ladder")))) return;
+
+        if (value.isPressed)
+        {
+            rigidBody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rigidBody.velocity.y);
+        rigidBody.velocity = playerVelocity;
+
+        animator.SetBool("isRunning", Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalSpeed)
+            transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), 1f); 
+    }
+
+    void ClimbLadder()
+    {
+        if (!collider.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            animator.SetBool("isClimbing", false);
+            rigidBody.gravityScale = originalGravity;
+            return;
+        }
+
+        bool playerHasVerticalSpeed = Mathf.Abs(rigidBody.velocity.y) > Mathf.Epsilon;
+        animator.SetBool("isClimbing", playerHasVerticalSpeed);
+
+        Vector2 climbVelocity = new Vector2(rigidBody.velocity.x, moveInput.y * climbSpeed);
+
+        rigidBody.velocity = climbVelocity;
+        rigidBody.gravityScale = 0;
+
+
     }
 }
